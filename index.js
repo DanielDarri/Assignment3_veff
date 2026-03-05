39b5cd3e672de4d1b9d1a6275e28f9c48393de6d
Daniel Darri <145784766+DanielDarri@users.noreply.github.com>
2026-03-05 12:14:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,81 @@ import { nextEventId, nextAttendeeId } from "./data/initialData.js";
 
 -------------------------- */
 
+const baseUrl = `${apiPath}${version}`;
+
+app.get(`${baseUrl}/events`, (req, res) => {
+  const allowedQueryParams = ['Name', 'Location'];
+  const queryKeys = Object.keys(req.query);
+
+  const hasInvalidQuery = queryKeys.some(
+    (key) => !allowedQueryParams.includes(key)
+  );
+
+  if (hasInvalidQuery) {
+    return res.status(400).json({
+      message: "Invalid query parameter provided"
+    });
+  }
+
+  let filteredEvents = events;
+
+  if (req.query.name) {
+    const nameFilter = req.query.name.toLocaleLowerCase();
+    filteredEvents = filteredEvents.filter((event) =>
+      event.name.toLocaleLowerCase().includes(nameFilter)
+    );
+  }
+
+  if (req.query.location) {
+    const locationFilter = req.query.location.toLocaleLowerCase();
+    filteredEvents = filteredEvents.filter((event) =>
+      event.location.toLocaleLowerCase().includes(locationFilter)
+    );
+  }
+
+  return res.status(200).json(filteredEvents)
+});
+
+app.get(`${baseUrl}/events/eventId`, (req, res) => {
+  const eventId = Number(req.params.eventId);
+
+  if (!Number.isInteger(eventId)) {
+    return res.status(400).json({
+      message: "Event id must be valid integer",
+    });
+  }
+
+  const event = events.find((e) => e.id === eventId);
+
+  if (!event) {
+    return res.status(404).json({
+      message: "Event not found",
+    });
+  }
+
+  const attendeeCount = attendees.filter((attendee) => 
+    attendee.eventIds.includes(eventId)
+  ).length;
+
+  const eventWithCount = {
+    ...event,
+    attendeeCount,
+  };
+
+  return res.status(200).json(eventWithCount);
+
+});
+
+
 /* --------------------------
 
     ATTENDEES ENDPOINTS    
 
 -------------------------- */
+
+app.get(`${baseUrl}/attendees`, (req, res) => {
+  return res.status(200).json(attendees);
+});
 
 /* --------------------------
 
